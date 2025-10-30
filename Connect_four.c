0b498bb258a4421f22392c021fcfe88085b80a1c
Ets <102168533+Eti-D@users.noreply.github.com>
2025-10-30 22:48:54 +0100

    Add files via upload

---
+++ Connect_four.c
@@ -0,0 +1,27 @@
+
+int Four_in_a_row(int width,int height, char[width][height]){
+    int Total_in_a_row =0;
+
+char 
+
+    int current_position[2];
+
+    int left[2]={-1,0};
+    int left_top[2]={-1,-1};
+    int top[2]={0,-1};
+    int top_right[2]={-1,1};
+    int right[2]={0,1};
+    
+    for(current_position[0]= height; current_position[0] >=0; current_position[0]--){
+        for(current_position[1] = width; current_position[1]>=0;current_position[1]--){
+		
+        }
+    }    
+}
+
+
+int main(void){
+
+
+
+}
