56e2cdd64854210b3bad6f61d280f87430a3c97b
Etis <102168533+EtisD@users.noreply.github.com>
2025-11-04 23:33:55 +0100

    Refactor Connect.c for game logic improvements

--- Connect.c
+++ Connect.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
-#define Width 7
+#define Width 6
 #define Height 6
 #define Dimension 2
 #define RED     "\x1b[31m"
 #define BLUE    "\x1b[34m"
-
 #define RESET   "\x1b[0m"
 
+/*
+int Four_in_a_row(char current_colour, int width,int height, char[width][height]){
+    int Total_in_a_row =0;
 
+    int current_position[Dimension] ={width, height};
+
+    int next_position[Dimension];
+
+    
+    int possible_moves[2][] = {
+        {-1,0},
+        {-1,-1},
+        {0,1},
+        {-1,1},
+        {0,1}
+    };
+    int current_movement[Dimension] = {0,0};
+
+    int movement_counter = 0;
+
+    while(Total_in_a_row<4 || (current_position[0] != 0 && current_position[1]!= 0)){
+        if(movement_counter==5){
+            movement_counter = 0;
+        }
+        next_position = {current_position[0] + possible_moves[movement_counter][0] , current_position[1] + possible_moves[movement_counter][1];
+
+
+    }
+
+}
+*/
 void print_game(char board[Width][Height]){
     
     for(int i = 0;i<Width;i++){
@@ -27,7 +56,6 @@ void print_game(char board[Width][Height]){
 
 
 void populate_board(char board[Width][Height]){
-
     for(int i = 0;i<Width;i++){
         for(int j = 0;j<Height;j++){
             board[i][j] = '0';
@@ -36,8 +64,17 @@ void populate_board(char board[Width][Height]){
 }
 
 
-void place(char colour,int column, char board[Width][Height]){
+int possible_next_move(int current_position[Dimension], int move[Dimension]){
+    for(int i=0; i<Dimension;i++){
+        if(current_position[i] + move[i]<0 || current_position[i] + move[i]){
+            return 0;
+        }
+    }
+}
 
+
+
+void place(char colour,int column, char board[Width][Height]){
     column --;
     int i = 0;
     while(i<7){
@@ -47,48 +84,52 @@ void place(char colour,int column, char board[Width][Height]){
     }
     board[i-1][column] = colour;
 }
-int check_row_counter(int row_count[6]){
-    for(int i = 0;i<7;i++){
-        if(row_count[i]<7)
-            return 1;
-        else 
-            return 0;
-    }
+
+int check_row_counter(int row_count[Width]){
+    int overloadedRow = -1;
+    for(int i = 0;i<Dimension;i++){
+        if(row_count[i]>=7){
+            overloadedRow = row_count[i];
+            break;
+        }
+        }
+    return overloadedRow;
+
 }
 
 
 int main(){
 
     char board[Width][Height];
+    int board_tracker[6] = {0,0,0,0,0,0};
+    int row_inputed = 9;
+    int player = 0;
+    char col;
     populate_board(board);
 
-
-    int input;
-    char col;
-    int counter = 1;
     while(1){
-        input = 9;
         print_game(board);
-        if(counter%2==0){
-            printf(BLUE"Enter your move: \n" RESET); 
-            col = 'b';}
-        else{
-            printf(RED"Enter your move: \n" RESET); 
+        
+        if(player%2==0){
+            printf(RED"Enter your move: \n" RESET);
             col = 'r';
         }
-        scanf("%d",&input);
-
-        
-        if(input<1){
-            printf("enter a number between 1 and 6");
-            counter--;
+        else{
+            col = 'b';
+            printf(BLUE"Enter your move: \n" RESET);
         }
+        scanf("%d",&row_inputed);
 
-        else if(input>7){
-            break;
+        if(row_inputed>0 && row_inputed<=7 && board_tracker[row_inputed-1]<=6){
+            board_tracker[row_inputed-1] ++;
+            place(col,row_inputed,board);
         }
-
-        place(col,input,board);
-        counter++;
+        else{
+            player --;
+            printf("\nEnter another number\n");
         }
+        player++;
+        printf("\n player: %d \n",player);
     }
+}
+
